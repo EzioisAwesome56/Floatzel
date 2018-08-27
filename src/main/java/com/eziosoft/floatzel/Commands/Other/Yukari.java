d29970acf4f9a7a0cedd4100ab18ea8dd3e0d647
Jonathan <jedijon567@gmail.com>
2018-08-27 19:48:47 -0400

    start adding the yukari command

---
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Yukari.java
@@ -0,0 +1,17 @@
+package com.eziosoft.floatzel.Commands.Other;
+
+import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class Yukari extends FCommand {
+    public Yukari(){
+        name = "yukari";
+        description = "sends a picture of the best waif-i mean girl";
+        category = other;
+    }
+
+    @Override
+    protected void execute(CommandEvent event){
+        v
+    }
+}
