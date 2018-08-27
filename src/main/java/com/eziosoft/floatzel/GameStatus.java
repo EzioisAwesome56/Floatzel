babedb872ac604b4ecaec312d4b3cbdbef6e1e0b
Jonathan <jedijon567@gmail.com>
2018-08-27 19:40:04 -0400

    Add some new games

--- src/main/java/com/eziosoft/floatzel/GameStatus.java
+++ src/main/java/com/eziosoft/floatzel/GameStatus.java
@@ -20,7 +20,8 @@ public class GameStatus extends TimerTask {
             "Extra gay", "memerino and the dank machine", "Dankform", "Half Life 4", "18 is a god", "SaltyPepper", "@here",
             "Simpsons", "Explosive cheese", "Being a bot", "hi Cublex", "Pentium 3", "meme machine", "18 is bisexual", "ezio is bi",
             "PyCharm", "Rectangle Gay", "Making the frogs gay", "eirrac", "what the fuck is a hedgehog", "heck you blur", "enitsirhc", "fuck me harder daddy",
-            "Intelij", "Smoking weed", "Persona -12 + GAY DLC", "Circles", "Godson is radical", "Ryan is gay", "Java 10"};
+            "Intelij", "Smoking weed", "Persona -12 + GAY DLC", "Circles", "Godson is radical", "Ryan is gay", "Java 10", "with Yukari", "Super Dankio Ocyssey",
+            "Fucking Mitsuru with a stick", "Persona: Waifu simulator"};
 
     @Override
     public void run() {
