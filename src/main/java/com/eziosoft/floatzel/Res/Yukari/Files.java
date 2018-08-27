c0c9ad78a25129ce6d05a3a7b4f4a4cfe3d4780d
Jonathan <jedijon567@gmail.com>
2018-08-27 19:58:53 -0400

    add a long ass file name list

--- src/main/java/com/eziosoft/floatzel/Res/Yukari/Files.java
+++ src/main/java/com/eziosoft/floatzel/Res/Yukari/Files.java
@@ -1,5 +1,8 @@
 package com.eziosoft.floatzel.Res.Yukari;
 
 public class Files {
-    // fill in the file names at some point
+    // file names
+    public static String[] yukaris = {"1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg",
+    "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.png", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg",
+    "20.jpg", "21.jpg"};
 }
