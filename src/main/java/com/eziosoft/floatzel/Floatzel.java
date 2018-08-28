208961b6a1dcec5a4179f0209bca328b04452d85
Jonathan <jedijon567@gmail.com>
2018-08-28 16:56:33 -0400

    version change
    im not ready to make 2.3

--- src/main/java/com/eziosoft/floatzel/Floatzel.java
+++ src/main/java/com/eziosoft/floatzel/Floatzel.java
@@ -41,7 +41,7 @@ public class Floatzel {
                 isdev = true;
             }
         }
-        version = !isdev ? "2.2.3" : "2.x Developement";
+        version = !isdev ? "2.3" : "2.x Developement";
          commandClient = new CommandClientBuilder().setOwnerId(Config.ownerid).useHelpBuilder(false).setPrefix(isdev ? Config.devprefix : Config.prefix).build();
 
 
