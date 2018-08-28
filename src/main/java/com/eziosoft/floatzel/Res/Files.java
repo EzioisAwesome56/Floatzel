6da681aac0e853c11e40b5c6247a2ed200dd75e9
Jonathan <jedijon567@gmail.com>
2018-08-28 16:33:04 -0400

    add more yukaris

--- src/main/java/com/eziosoft/floatzel/Res/Files.java
+++ src/main/java/com/eziosoft/floatzel/Res/Files.java
@@ -4,7 +4,7 @@ public class Files {
     // file names
     public static String[] yukaris = {"1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg",
     "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.png", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg",
-    "20.jpg", "21.jpg"};
+    "20.jpg", "21.jpg", "22.jpg", "23.jpg"};
 
     // simply put - make a list of all the file names
     public static String[] unocards = {"card1.png", "card2.png", "card3.jpg", "card4.png", "card5.png", "card6.png"};
