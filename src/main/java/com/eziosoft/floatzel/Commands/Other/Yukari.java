d5d2382648adb0dd8789a112e238bdaae643be5a
Jonathan <jedijon567@gmail.com>
2018-08-27 20:12:51 -0400

    basically reuse a lot of Reverse

--- src/main/java/com/eziosoft/floatzel/Commands/Other/Yukari.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Yukari.java
@@ -1,7 +1,15 @@
 package com.eziosoft.floatzel.Commands.Other;
 
 import com.eziosoft.floatzel.Commands.FCommand;
+import com.eziosoft.floatzel.Res.Uno.Uno;
+import com.eziosoft.floatzel.Res.Yukari.Files;
 import com.jagrosh.jdautilities.command.CommandEvent;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 public class Yukari extends FCommand {
     public Yukari(){
@@ -12,6 +20,22 @@ public class Yukari extends FCommand {
 
     @Override
     protected void execute(CommandEvent event){
-        // start doing thingos soon
+        // im lazy, so copy pasta
+        Random random = new Random();
+        BufferedImage img = null;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        String filepath = "";
+        // generate a number
+        int card = random.nextInt(Files.yukaris.length);
+        // get get yukari
+        try {
+            img = ImageIO.read(Uno.class.getResource(Files.yukaris[card]));
+            ImageIO.setUseCache(false);
+            stream.flush();
+            ImageIO.write(img, "png", stream);
+            event.getChannel().sendFile(stream.toByteArray(), "yukari.png", null).queue();
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
     }
 }
