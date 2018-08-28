d5e010e8601d6bc9b7f1cee8a410008becb77779
Jonathan <jedijon567@gmail.com>
2018-08-28 17:48:35 -0400

    oops

--- src/main/java/com/eziosoft/floatzel/Util/Database.java
+++ src/main/java/com/eziosoft/floatzel/Util/Database.java
@@ -25,7 +25,7 @@ public class Database {
     public static File bldir = new File(bloan);
     public static String ext = ".db";
     // new sqlite database location, stored as a file
-    public static String sqlthing = System.getProperty("user.dir") + "floatzel.db";
+    public static String sqlthing = System.getProperty("user.dir") + "/floatzel.db";
     public static File sqldb = new File(sqlthing);
     // actual url to the db
     public static String url = "jdbc:sqlite:" + sqlthing;
