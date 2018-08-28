b87b6ecca3a362f21ed920c1ae7dd3e8f77e9f9f
Jonathan <jedijon567@gmail.com>
2018-08-28 17:19:07 -0400

    write basic database init code

--- src/main/java/com/eziosoft/floatzel/Util/Database.java
+++ src/main/java/com/eziosoft/floatzel/Util/Database.java
@@ -2,6 +2,10 @@ package com.eziosoft.floatzel.Util;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Database {
     // set the root db path
@@ -40,10 +44,21 @@ public class Database {
     }
 
     // SQLITE init function
-    public static void sqlinit(){
+    public static void sqlinit() {
         // file path i think
-        String url = System.getProperty("user.dir");
-        System.out.println(url);
+        String url = "jdbc:sqlite:" + System.getProperty("user.dir") + "floatzel.db";
+        // copy paste from a website
+        // sqlite database creation tool
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     // check if db entry exists
