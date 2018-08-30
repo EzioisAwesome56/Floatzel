7a481033797e0c8d802f0c9ce4dcdd46667e136e
jonathan <jedijon567@gmail.com>
2018-08-30 00:12:11 -0400

    add even more yukaris credit 2 ecumber for finding these

--- src/main/java/com/eziosoft/floatzel/Res/Files.java
+++ src/main/java/com/eziosoft/floatzel/Res/Files.java
@@ -6,7 +6,7 @@ public class Files {
     "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.png", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg",
     "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.png", "25.jpg", "26.jpg", "27.png", "28.jpg", "29.jpeg", "30.jpg",
     "31.jpg", "32.jpg", "33.png", "34.png", "35.png", "36.jpg", "37.jpg", "38.png", "39.png", "40.png", "41.jpeg",
-    "42.jpg", "43.png"};
+    "42.jpg", "43.png", "44.png", "45.png", "46.png"};
 
     // simply put - make a list of all the file names
     public static String[] unocards = {"card1.png", "card2.png", "card3.jpg", "card4.png", "card5.png", "card6.png"};
