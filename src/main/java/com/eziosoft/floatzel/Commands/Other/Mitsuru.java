196d54d992f0a1bc0dbc628ad2dd55ae8d39cdf9
Jonathan <jedijon567@gmail.com>
2018-08-29 20:04:38 -0400

    finish command

--- src/main/java/com/eziosoft/floatzel/Commands/Other/Mitsuru.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Mitsuru.java
@@ -1,6 +1,43 @@
 package com.eziosoft.floatzel.Commands.Other;
 
 import com.eziosoft.floatzel.Commands.FCommand;
+import com.eziosoft.floatzel.Res.Files;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 public class Mitsuru extends FCommand {
+    public Mitsuru(){
+        name = "mitsuru";
+        description = "sends a picture of mitsuru from persona 3";
+        category = waifu;
+    }
+
+    @Override
+    protected void execute(CommandEvent event){
+        // copy paste form yukari.class
+        Random random = new Random();
+        BufferedImage img = null;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        String filepath = "";
+        // generate a number
+        int card = random.nextInt(Files.yukaris.length);
+        // get photo
+        try {
+            img = ImageIO.read(Files.class.getResource("/mit/"+ Files.mitsurus[card]));
+            ImageIO.setUseCache(false);
+            stream.flush();
+            ImageIO.write(img, "png", stream);
+            //unimplimeted feature
+            //event.getChannel().sendMessage(Uno.unorep[random.nextInt(Uno.unorep.length)]).queue();
+            event.getChannel().sendFile(stream.toByteArray(), "mitsuru.png", null).queue();
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
