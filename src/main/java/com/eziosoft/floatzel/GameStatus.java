b3c88147337474d21aa9b1e57ce97ca0771aee16
jonathan <jedijon567@gmail.com>
2018-08-30 00:10:27 -0400

    new game

--- src/main/java/com/eziosoft/floatzel/GameStatus.java
+++ src/main/java/com/eziosoft/floatzel/GameStatus.java
@@ -21,7 +21,7 @@ public class GameStatus extends TimerTask {
             "Simpsons", "Explosive cheese", "Being a bot", "hi Cublex", "Pentium 3", "meme machine", "18 is bisexual", "ezio is bi",
             "PyCharm", "Rectangle Gay", "Making the frogs gay", "eirrac", "what the fuck is a hedgehog", "heck you blur", "enitsirhc", "fuck me harder daddy",
             "Intelij", "Smoking weed", "Persona -12 + GAY DLC", "Circles", "Godson is radical", "Ryan is gay", "Java 10", "with Yukari", "Super Dankio Ocyssey",
-            "Fucking Mitsuru with a stick", "Persona: Waifu simulator", "Ezio X Yukari"};
+            "Fucking Mitsuru with a stick", "Persona: Waifu simulator", "Ezio X Yukari", "Yukari is hot"};
 
     @Override
     public void run() {
