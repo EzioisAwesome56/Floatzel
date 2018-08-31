4f921c943802222d9b0f44b2ceaf19b1b9727225
Jonathan <jedijon567@gmail.com>
2018-08-30 21:22:39 -0400

    do some shit with it

--- src/main/java/com/eziosoft/floatzel/Commands/Currency/Check.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Currency/Check.java
@@ -1,6 +1,19 @@
 package com.eziosoft.floatzel.Commands.Currency;
 
 import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
 
 public class Check extends FCommand {
+    public Check(){
+        name = "check";
+        description = "check's the bank account of another user";
+        category = money;
+    }
+
+
+
+    @Override
+    protected void execute(CommandEvent event) {
+
+    }
 }
