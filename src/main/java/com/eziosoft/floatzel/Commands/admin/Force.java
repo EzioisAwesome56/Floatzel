37702257dc8e71b862a8d92d0302c8b2d773944b
jonathan <jedijon567@gmail.com>
2018-11-04 23:22:59 -0500

    new force command

--- src/main/java/com/eziosoft/floatzel/Commands/admin/Force.java
+++ src/main/java/com/eziosoft/floatzel/Commands/admin/Force.java
@@ -1,4 +1,25 @@
 package com.eziosoft.floatzel.Commands.admin;
 
-public class Force {
+import com.eziosoft.floatzel.Commands.FCommand;
+import com.eziosoft.floatzel.Util.StockUtil;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class Force extends FCommand {
+    public Force(){
+        name = "force";
+        description = "Forces a event to take place";
+        category = owner;
+    }
+
+    @Override
+    protected void execute(CommandEvent event){
+        String arg = event.getArgs();
+        if (arg == "stock"){
+            // force a stock update
+            StockUtil.updateStock();
+            event.getChannel().sendMessage("Forced stock update!").queue();
+        } else {
+            event.getChannel().sendMessage("Unknown argument!").queue();
+        }
+    }
 }
