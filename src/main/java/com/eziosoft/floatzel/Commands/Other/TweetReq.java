03c6e4ab2c4ca5667fdb89eb62960230275e1cef
jonathan <jedijon567@gmail.com>
2018-11-09 18:54:40 -0500

    halfass finish this

--- src/main/java/com/eziosoft/floatzel/Commands/Other/TweetReq.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/TweetReq.java
@@ -1,6 +1,15 @@
 package com.eziosoft.floatzel.Commands.Other;
 
 import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
 
 public class TweetReq extends FCommand {
+    public TweetReq(){
+        name = "a";
+    }
+
+    @Override
+    protected void cmdrun(CommandEvent event){
+        event.getChannel().sendMessage("re").queue();
+    }
 }
