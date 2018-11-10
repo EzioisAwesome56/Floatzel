d854332681a5897af75036419794008d9ecc20dd
jonathan <jedijon567@gmail.com>
2018-11-10 17:23:45 -0500

    work on error handler more

--- src/main/java/com/eziosoft/floatzel/Util/ErrorCatcher.java
+++ src/main/java/com/eziosoft/floatzel/Util/ErrorCatcher.java
@@ -2,8 +2,22 @@ package com.eziosoft.floatzel.Util;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Random;
+
 public class ErrorCatcher {
+
+    private static Random random = new Random();
+
     public static void CatchError(String e, CommandEvent event){
-        // catch tme errors
+        //setup a byte array
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        String output = null;
+        // pick a phrase to slap at the top of the file
+        String[] phrases = { "Aw shit!","Damn it ezio, you suck at coding","Crash party, anyone?","Salty is dumb lol",
+        "how did you find this?","Stop breaking me, damn it!", "fuck off"};
+        int index = random.nextInt(phrases.length);
+        output = output + phrases[index] + "\n\n";
+
     }
 }
