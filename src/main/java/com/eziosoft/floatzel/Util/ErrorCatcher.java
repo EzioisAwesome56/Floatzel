83c9135d6ba79bc7af808ae84d6def10acc44c81
jonathan <jedijon567@gmail.com>
2018-11-10 17:14:37 -0500

    add new error catcher

--- src/main/java/com/eziosoft/floatzel/Util/ErrorCatcher.java
+++ src/main/java/com/eziosoft/floatzel/Util/ErrorCatcher.java
@@ -1,4 +1,9 @@
 package com.eziosoft.floatzel.Util;
 
+import com.jagrosh.jdautilities.command.CommandEvent;
+
 public class ErrorCatcher {
+    public static void CatchError(String e, CommandEvent event){
+        // catch tme errors
+    }
 }
