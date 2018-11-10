9ed65f8ea6a932717ecb5c5638fe8f9facef840b
jonathan <jedijon567@gmail.com>
2018-11-10 14:28:59 -0500

    h

--- src/main/java/com/eziosoft/floatzel/Commands/Debug/Debug.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Debug/Debug.java
@@ -18,40 +18,7 @@ public class Debug extends FCommand {
 
     @Override
     protected void cmdrun(CommandEvent event){
-        String raw = getUrlContents("https://api.duckduckgo.com/?q=DuckDuckGo&format=json");
-        event.getChannel().sendMessage(raw).queue();
+        event.getChannel().sendMessage("error: nothing").queue();
     }
 
-    private static String getUrlContents(String theUrl)
-    {
-        StringBuilder content = new StringBuilder();
-
-        // many of these calls can throw exceptions, so i've just
-        // wrapped them all in one try/catch statement.
-        try
-        {
-            // create a url object
-            URL url = new URL(theUrl);
-
-            // create a urlconnection object
-            URLConnection urlConnection = url.openConnection();
-
-            // wrap the urlconnection in a bufferedreader
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-            String line;
-
-            // read from the urlconnection via the bufferedreader
-            while ((line = bufferedReader.readLine()) != null)
-            {
-                content.append(line + "\n");
-            }
-            bufferedReader.close();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        return content.toString();
-    }
 }
