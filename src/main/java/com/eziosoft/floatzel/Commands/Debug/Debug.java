344bd651a306753dd3d3f27ccab4296e1ffc50ad
jonathan <jedijon567@gmail.com>
2018-11-10 14:14:10 -0500

    debug 2

--- src/main/java/com/eziosoft/floatzel/Commands/Debug/Debug.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Debug/Debug.java
@@ -1,4 +1,17 @@
 package com.eziosoft.floatzel.Commands.Debug;
 
-public class Debug {
+import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class Debug extends FCommand {
+    public Debug(){
+        name = "debug";
+        help = "Debugs whatever is coded in";
+        category = owner;
+    }
+
+    @Override
+    protected void cmdrun(CommandEvent event){
+        // stuff
+    }
 }
