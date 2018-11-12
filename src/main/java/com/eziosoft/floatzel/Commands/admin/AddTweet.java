5ee506f11ebc67896853703f2067c44af454bf89
jonathan <jedijon567@gmail.com>
2018-11-12 18:34:09 -0500

    more shit

--- src/main/java/com/eziosoft/floatzel/Commands/admin/AddTweet.java
+++ src/main/java/com/eziosoft/floatzel/Commands/admin/AddTweet.java
@@ -1,4 +1,12 @@
 package com.eziosoft.floatzel.Commands.admin;
 
-public class AddTweet {
+import com.eziosoft.floatzel.Commands.FCommand;
+
+public class AddTweet extends FCommand {
+    public AddTweet(){
+        name = "addtweet";
+        description = "Adds a tweet to the tweet database";
+        ownerCommand = true;
+        category = owner;
+    }
 }
