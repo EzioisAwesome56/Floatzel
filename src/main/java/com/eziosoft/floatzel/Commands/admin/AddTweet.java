2d4e028d761a850cd9e463e1d1127fd4e198f922
jonathan <jedijon567@gmail.com>
2018-11-12 18:37:08 -0500

    write more of the command's code

--- src/main/java/com/eziosoft/floatzel/Commands/admin/AddTweet.java
+++ src/main/java/com/eziosoft/floatzel/Commands/admin/AddTweet.java
@@ -1,6 +1,7 @@
 package com.eziosoft.floatzel.Commands.admin;
 
 import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
 
 public class AddTweet extends FCommand {
     public AddTweet(){
@@ -8,5 +9,15 @@ public class AddTweet extends FCommand {
         description = "Adds a tweet to the tweet database";
         ownerCommand = true;
         category = owner;
+    }
+
+    @Override
+    protected void cmdrun(CommandEvent event){
+        String tweet = event.getArgs();
+        if (tweet.length() > 240){
+            event.getChannel().sendMessage("That tweet is too long dumbass!").queue();
+            return;
+        }
+        // save the tweet
     }
 }
