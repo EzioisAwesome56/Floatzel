3e853d513a79e37f1d9687d5bfafc5610cee9979
jonathan <jedijon567@gmail.com>
2018-11-12 19:04:20 -0500

    more eval shit

--- src/main/java/com/eziosoft/floatzel/Commands/Owner/Eval.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Owner/Eval.java
@@ -1,4 +1,18 @@
 package com.eziosoft.floatzel.Commands.Owner;
 
-public class Eval {
+import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class Eval extends FCommand {
+    public Eval(){
+        name = "eval";
+        help = "runs provided statement";
+        ownerCommand = true;
+        category = owner;
+    }
+
+    @Override
+    protected void cmdrun(CommandEvent event){
+        //re
+    }
 }
