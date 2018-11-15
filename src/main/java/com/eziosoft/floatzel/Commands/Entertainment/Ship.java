0fdbe186b8cd6fa9b8d0a43553cdeb4ded56a53b
Jonathan <jedijon567@gmail.com>
2018-11-15 17:34:27 -0500

    finish this command later

--- src/main/java/com/eziosoft/floatzel/Commands/Entertainment/Ship.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Entertainment/Ship.java
@@ -1,4 +1,26 @@
 package com.eziosoft.floatzel.Commands.Entertainment;
 
-public class Ship {
+import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class Ship extends FCommand {
+    public Ship(){
+        name = "ship";
+        description = "make your love dreams come true!";
+        category = fun;
+    }
+
+    @Override
+    protected void cmdrun(CommandEvent event){
+        String one = "";
+        String two = "";
+        try {
+            one = argsplit[0];
+            two = argsplit[1];
+        } catch (ArrayIndexOutOfBoundsException e){
+            event.getChannel().sendMessage("Error: you didnt provide enough arguments dumb fuck").queue();
+            return;
+        }
+        event.getChannel().sendMessage("Unfinished command! Please wait for a future update").queue();
+    }
 }
