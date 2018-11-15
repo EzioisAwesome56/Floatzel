c5fa05f281fc75ad92f446429be9355917d860ea
Jonathan <jedijon567@gmail.com>
2018-11-15 15:10:50 -0500

    start adding a big wall of checks

--- src/main/java/com/eziosoft/floatzel/Commands/PayForCommands/Tag.java
+++ src/main/java/com/eziosoft/floatzel/Commands/PayForCommands/Tag.java
@@ -2,6 +2,7 @@ package com.eziosoft.floatzel.Commands.PayForCommands;
 
 import com.eziosoft.floatzel.Commands.FCommand;
 import com.eziosoft.floatzel.Util.Database;
+import com.eziosoft.floatzel.Util.Error;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
 public class Tag extends FCommand {
@@ -14,6 +15,9 @@ public class Tag extends FCommand {
     @Override
     protected void cmdrun(CommandEvent event){
         String args = argsplit[0];
+        String tn = null;
+        int howlong = argsplit.length;
+        System.out.println(howlong);
         boolean perm = Database.dbcheckiftag(event.getGuild().getId());
         if(args.equals("check")){
             if(!perm){
@@ -40,12 +44,28 @@ public class Tag extends FCommand {
                 return;
             }
         } else if (args.equals("add")){
+            try {
+                tn = argsplit[1];
+            } catch (NullPointerException e){
+                Error.Catch(e);
+                return;
+            }
             // run checks before actually adding the tag
             if (!perm){
                 event.getChannel().sendMessage("Error: this guild has not bought the tag command!").queue();
                 return;
             } else if (!Database.dbcheckifexist(event.getAuthor().getId())){
                 event.getChannel().sendMessage("Error: adding a tag costs 50"+moneyicon+", You have 0"+moneyicon+"!").queue();
+                return;
+            } else if(Database.dbchecktag(event.getGuild().getId(), tn)){
+                event.getChannel().sendMessage("Error: a tag with that name already exists!").queue();
+                return;
+            } else if(howlong < 3){
+                event.getChannel().sendMessage("Error: you did not specify tag contents dumbass!").queue();
+                return;
+            } else if (Database.dbloadint(event.getAuthor().getId()) < 50){
+                String curbal = Integer.toString((Database.dbloadint(event.getAuthor().getId())));
+                event.getChannel().sendMessage("Error: adding a tag costs 50"+moneyicon+", You have "+curbal+moneyicon+"!").queue();
                 return;
             }
         }
