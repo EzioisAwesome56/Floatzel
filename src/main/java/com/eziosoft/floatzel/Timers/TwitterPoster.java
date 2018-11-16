5082feb171aa04e8146a32cd54e674094fab5620
Jonathan <jedijon567@gmail.com>
2018-11-15 19:46:02 -0500

    finsih the tweetbot

--- src/main/java/com/eziosoft/floatzel/Timers/TwitterPoster.java
+++ src/main/java/com/eziosoft/floatzel/Timers/TwitterPoster.java
@@ -2,6 +2,7 @@ package com.eziosoft.floatzel.Timers;
 
 import com.eziosoft.floatzel.Floatzel;
 import com.eziosoft.floatzel.Util.Database;
+import com.eziosoft.floatzel.Util.Utils;
 import com.rethinkdb.net.Cursor;
 
 import java.util.List;
@@ -16,24 +17,7 @@ public class TwitterPoster extends TimerTask {
 
     @Override
     public void run(){
-        // is the tweetbot on?
-        if (!Floatzel.tweeton){
-            return;
-        }
-        // get all ttweets
-        Cursor cur = Database.dbgetalltweets();
-        // did it fail to load?
-        if (Floatzel.fail){
-            Floatzel.fail = false;
-            return;
-        }
-        // get a list from the cursor
-        List tweets = cur.toList();
-        int lenght = tweets.size();
-        // then get a tweet from the list
-        int oof = random.nextInt(lenght);
-        String tweet = tweets.get(oof).toString();
-        // then send the tweet out to the world
-
+        // just call the tweetbot in utils
+        Utils.tweetbot();
     }
 }
--- src/main/java/com/eziosoft/floatzel/Util/Utils.java
+++ src/main/java/com/eziosoft/floatzel/Util/Utils.java
@@ -1,5 +1,6 @@
 package com.eziosoft.floatzel.Util;
 
+import com.eziosoft.floatzel.Floatzel;
 import com.rethinkdb.net.Cursor;
 import org.apache.commons.lang3.StringUtils;
 
@@ -83,5 +84,27 @@ public class Utils {
             Error.Catch(e);
             return "ERROR IN getValue!";
         }
+    }
+
+    public static void tweetbot(){
+        // is the tweetbot on?
+        if (!Floatzel.tweeton){
+            return;
+        }
+        // get all ttweets
+        Cursor cur = Database.dbgetalltweets();
+        // did it fail to load?
+        if (Floatzel.fail){
+            Floatzel.fail = false;
+            return;
+        }
+        // get a list from the cursor
+        List tweets = cur.toList();
+        int lenght = tweets.size();
+        // then get a tweet from the list
+        int oof = random.nextInt(lenght);
+        String tweet = tweets.get(oof).toString();
+        // then send the tweet out to the world
+        TwitterManager.tweet(tweet);
     }
 }
