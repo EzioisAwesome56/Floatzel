0d1cd2ee7379cda0852226fe77995d20e6b38b52
Jonathan <jedijon567@gmail.com>
2018-11-19 18:33:09 -0500

    add first useful thing to eval fun

--- src/main/java/com/eziosoft/floatzel/EFun.java
+++ src/main/java/com/eziosoft/floatzel/EFun.java
@@ -1,4 +1,12 @@
 package com.eziosoft.floatzel;
 
+import com.eziosoft.floatzel.Util.TwitterUtils;
+
 public class EFun {
+
+    public static void tweet(){
+        // just run the tweetbot
+        TwitterUtils.tweetbot();
+        return;
+    }
 }
