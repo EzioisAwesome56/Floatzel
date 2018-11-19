d0ab3a611f9863206d9c4b10e87fe79a23678a72
Jonathan <jedijon567@gmail.com>
2018-11-19 18:19:54 -0500

    make it work or something...tm

--- src/main/java/com/eziosoft/floatzel/Commands/Other/Floof.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Floof.java
@@ -1,7 +1,15 @@
 package com.eziosoft.floatzel.Commands.Other;
 
 import com.eziosoft.floatzel.Commands.FCommand;
+import com.eziosoft.floatzel.Res.Files;
+import com.eziosoft.floatzel.Util.Error;
 import com.jagrosh.jdautilities.command.CommandEvent;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 public class Floof extends FCommand {
     public Floof(){
@@ -12,6 +20,25 @@ public class Floof extends FCommand {
 
     @Override
     protected void cmdrun(CommandEvent event){
-        // do a thing
+        // reuse the same old code for a 3rd time
+        Random random = new Random();
+        BufferedImage img = null;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        String filepath = "";
+        // generate a number
+        int card = random.nextInt(Files.floof.length);
+        // get dat floofy boi
+        try {
+            img = ImageIO.read(Files.class.getResource("/floof/"+ Files.floof[card]));
+            ImageIO.setUseCache(false);
+            stream.flush();
+            ImageIO.write(img, "png", stream);
+            //unimplimeted feature
+            //event.getChannel().sendMessage(Uno.unorep[random.nextInt(Uno.unorep.length)]).queue();
+            event.getChannel().sendFile(stream.toByteArray(), "floofboi.png", null).queue();
+            stream.close();
+        } catch (IOException e) {
+            Error.Catch(e);
+        }
     }
 }
