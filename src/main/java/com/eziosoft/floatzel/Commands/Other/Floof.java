66463d28aee4ef361b33112b3c9a5d74a30337c0
Jonathan <jedijon567@gmail.com>
2018-11-19 17:38:44 -0500

    wow

--- src/main/java/com/eziosoft/floatzel/Commands/Other/Floof.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Floof.java
@@ -1,4 +1,17 @@
 package com.eziosoft.floatzel.Commands.Other;
 
-public class Floof {
+import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class Floof extends FCommand {
+    public Floof(){
+        name = "floof";
+        help = "Floof up your day!";
+        category =  other;
+    }
+
+    @Override
+    protected void cmdrun(CommandEvent event){
+        // do a thing
+    }
 }
