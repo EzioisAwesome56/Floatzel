7e420ede909485af28ec11225090f53d8041114c
Jonathan <jedijon567@gmail.com>
2018-11-19 19:00:40 -0500

    add some new phrases to tweet later

--- src/main/java/com/eziosoft/floatzel/Res/Phrase.java
+++ src/main/java/com/eziosoft/floatzel/Res/Phrase.java
@@ -1,4 +1,7 @@
 package com.eziosoft.floatzel.Res;
 
 public class Phrase {
+
+    public static String[] floofword = {"You get a floof", "Dont touch the floof please", "Floofy bois are good", "Dont eat the floof",
+    "Warning: Cuteness is explosive", "His feet smell like shit...I think", "Okay i lied his feet smell alright"};
 }
