ee92c74f8d814c2e4e8421b2c0169b0f419d1b6d
Jonathan <jedijon567@gmail.com>
2018-11-25 16:56:50 -0500

    make randcourse use new api syntax

--- src/main/java/com/eziosoft/floatzel/Commands/Smm/RandCourse.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Smm/RandCourse.java
@@ -2,10 +2,11 @@ package com.eziosoft.floatzel.Commands.Smm;
 
 import com.eziosoft.floatzel.Commands.FCommand;
 import com.eziosoft.floatzel.Util.Utils;
+import com.eziosoft.smm4j.Level;
 import com.eziosoft.smm4j.Smm4j;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
-import static com.eziosoft.smm4j.Random.GetRandomLevel;
+import static com.eziosoft.smm4j.Util.GetRandomLevelID;
 
 public class RandCourse extends FCommand {
     public RandCourse(){
@@ -17,13 +18,13 @@ public class RandCourse extends FCommand {
     @Override
     protected void cmdrun(CommandEvent event){
         event.getChannel().sendTyping().queue();
-        String id = GetRandomLevel();
+        String id = GetRandomLevelID();
         if (id.equals("error")){
             event.getChannel().sendMessage("Smm4j encountered an error! You are probably being rated limited by nintendo. Try again in an hour").queue();
             return;
         }
         // then get level information
-        String[] levelinf = Smm4j.getLevel(id);
+        Level levelinf = Smm4j.getLevel(id);
         event.getChannel().sendMessage(Utils.buildSmm(levelinf)).queue();
     }
 }
