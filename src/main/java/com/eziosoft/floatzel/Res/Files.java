f48d6790542147cbef52ba3c95fae92352784736
Jonathan <jedijon567@gmail.com>
2018-11-30 18:57:23 -0500

    ralsei dump holy shit

--- src/main/java/com/eziosoft/floatzel/Res/Files.java
+++ src/main/java/com/eziosoft/floatzel/Res/Files.java
@@ -22,5 +22,7 @@ public class Files {
 
     // file names for new thing
     public static String[] floof = {"1.png", "2.png", "3.png", "4.png", "5.jpg", "6.png", "7.png", "8.png", "9.png", "10.png", "11.jpg", "13.jpg", "14.png", "15.png", "12.png",
-    "16.png", "17.jpg", "18.jpg", "19.jpg", "20.png", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.gif"};
+    "16.png", "17.jpg", "18.jpg", "19.jpg", "20.png", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.gif", "27.png", "28.png", "29.jpg", "30.png", "31.jpg", "32.jpg",
+    "33.png", "34.jpg", "35.mp4", "36.jpg", "37.png", "38.gif", "39.png", "40.png", "41.png", "42.jpg", "43.png", "44.jpg", "45.jpg", "46.png", "47.png", "48.jpg",
+    "49.jpg", "50.jpg", "51.jpg", "52.png", "53.jpg", "54.jpg", "55.png", "56.png", "57.png", "58.png", "59.png", "60.gif", "61.jpg", "62.png"};
 }
