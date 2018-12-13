d58b2f97d25ff366b1051f3fea8a09773c63ad57
Jonathan <jedijon567@gmail.com>
2018-12-13 18:00:36 -0500

    Version bump

--- src/main/java/com/eziosoft/floatzel/Floatzel.java
+++ src/main/java/com/eziosoft/floatzel/Floatzel.java
@@ -53,7 +53,7 @@ public class Floatzel {
                 isdev = true;
             }
         }
-        version = !isdev ? "2.4.3" : "2.x Developement";
+        version = !isdev ? "2.4.3 HOTFIX 1" : "2.x Developement";
          commandClient = new CommandClientBuilder().setOwnerId(Config.ownerid).useHelpBuilder(false).setPrefix(isdev ? Config.devprefix : Config.prefix).build();
 
 
