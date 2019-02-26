31f0b3e9d070842c58847b2982ca81dcebb02dca
jon <jedijon567@gmail.com>
2019-02-25 19:37:59 -0500

    more work on cowsay

--- src/main/java/com/eziosoft/floatzel/Commands/Other/Cow.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Cow.java
@@ -15,8 +15,29 @@ public class Cow extends FCommand {
 
     @Override
     protected void cmdrun(CommandEvent event) {
+        int length;
+        int tick = 0;
         if (StringUtils.isEmpty(event.getArgs())){
             event.reply("What the fuck man, you gave me no arguments!!!!!!!!!!!!!!!!!");
+            return;
+        }
+        // check to see if -f was provided
+        if (argsplit[0].equals("-f")){
+            if (argsplit.length == 1){
+                event.reply("Fuckface, you didnt provide enough arguments!");
+                return;
+            }
+            // okay so clearly we have something
+            length = argsplit.length - 2;
+            StringBuilder builder = new StringBuilder();
+            while (tick != length){
+                builder.append(argsplit[2+tick]);
+                builder.append(" ");
+                tick++;
+            }
+            // then form the message
+            String[] wat = {"-f", argsplit[1], builder.toString()};
+            event.reply("```\n"+Cowsay.say(wat)+"\n```");
             return;
         }
         String[] wat = {"-f", "tux", event.getArgs()};
