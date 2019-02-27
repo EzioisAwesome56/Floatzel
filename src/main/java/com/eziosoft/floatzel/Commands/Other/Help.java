8df57584f0451b21e8cf7c00299193b42b727087
jon <jedijon567@gmail.com>
2019-02-26 20:34:19 -0500

    this doesnt work and idk why

--- src/main/java/com/eziosoft/floatzel/Commands/Other/Help.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Help.java
@@ -6,6 +6,7 @@ import com.eziosoft.floatzel.Floatzel;
 import com.eziosoft.floatzel.Util.Utils;
 import com.eziosoft.floatzel.kekbot.EmbedPaginator;
 import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.core.EmbedBuilder;
 import org.apache.commons.lang3.ObjectUtils;
@@ -126,22 +127,7 @@ public class Help extends FCommand {
     //Roughly stolen from https://github.com/Godson777/KekBot/blob/master/src/main/java/com/godson/kekbot/command/commands/general/Help.java
     // Sorry, godson!
     private void sendHelp(CommandEvent event){
-        // reused floatzel code
-        List<String> rawcats = new ArrayList<String>();
-        // go through all commands and form a category list
-        commands.forEach(cmd -> {
-            if (cmd.getCategory() != null) {
-                String cmdcat = cmd.getCategory().getName();
-                if (!rawcats.contains(cmdcat)) {
-                    rawcats.add((String) cmdcat);
-                }
-            }
-        });
-        // new code
-        // form a string builder
-        StringBuilder buildbot = new StringBuilder();
-        // Stolen from kekbot
-
+        List<Category> categories = event.getClient().getCommands().stream().map(Command::getCategory).distinct().sorted(Comparator.comparing(Category::getName)).collect(Collectors.toList());
         EmbedPaginator.Builder builder = new EmbedPaginator.Builder();
         builder.addUsers(event.getAuthor());
         builder.setEventWaiter(Floatzel.waiter);
@@ -149,30 +135,19 @@ public class Help extends FCommand {
         builder.waitOnSinglePage(true);
         builder.showPageNumbers(true);
 
-        // more stolen kekbot code
-        // what is this, kekfloatzel????
-        rawcats.forEach(c -> {
-            if (c.equals(FCommand.owner.getName()) && !event.isOwner()) return;
-            /*
-            if (c.getName().equalsIgnoreCase("bot admin") && !Utils.isAdmin(event.getAuthor().getId())) return;
+        categories.forEach(c -> {
+            if (c.getName().equalsIgnoreCase("bot owner") && !event.isOwner()) return;
+            if (c.getName().equalsIgnoreCase("bot admin") && !Utils.isAdmin(event.getAuthor().getId()
+            )) return;
             if (c.getName().equalsIgnoreCase("test")) return;
-            if (c.getName().equals("unassigned")) return;*/
+            if (c.getName().equals("unassigned")) return;
+            List<Command> commands = new ArrayList<>(event.getClient().getCommands()).stream().filter(cmd -> cmd.getCategory().equals(c)).sorted(Comparator.comparing(Command::getName)).collect(Collectors.toList());
             for (int i = 0; i < commands.size(); i += 10) {
-                List<FCommand> currentPage = commands.subList(i, (i + 10 < commands.size() ? i + 10 : commands.size()));
+                List<Command> currentPage = commands.subList(i, (i + 10 < commands.size() ? i + 10 : commands.size()));
                 EmbedBuilder eBuilder = new EmbedBuilder();
-                eBuilder.setTitle(c.toString());
-                // floatzel code!!!!!!
-                // dealing with commands in a way i understand
-                currentPage.forEach(cmd -> {
-                    if (cmd.getCategory() != null) {
-                        if (cmd.getCategory().getName().equals(FCommand.other.getName())) {
-                            buildbot.append("what" + cmd.getName() + " - " + cmd.getHelp() + "\n");
-                        }
-                    }
-                });
-                eBuilder.setDescription(buildbot.toString());
-                // now back to kekbot
-                eBuilder.setFooter("Floatzel v" + Floatzel.version, null);
+                eBuilder.setTitle(c.getName());
+                eBuilder.setDescription(StringUtils.join(currentPage.stream().map(cmd -> Floatzel.isdev ? Config.devprefix : Config.prefix + cmd.getName() + " - " + cmd.getHelp()).collect(Collectors.toList()), "\n"));
+                eBuilder.setFooter("KekBot v" + Floatzel.version, null);
                 eBuilder.setAuthor("KekBot, your friendly meme-based bot!", null, event.getSelfUser().getAvatarUrl());
                 builder.addItems(eBuilder.build());
             }
