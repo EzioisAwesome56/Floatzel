e1fc994bc5cb96a25c445e7737df50c12c557b86
jon <jedijon567@gmail.com>
2019-02-27 20:33:30 -0500

    finish this class

--- src/main/java/com/eziosoft/floatzel/JsonConfig.java
+++ src/main/java/com/eziosoft/floatzel/JsonConfig.java
@@ -1,5 +1,8 @@
 package com.eziosoft.floatzel;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class JsonConfig {
     // tokens
     String token;
@@ -7,4 +10,27 @@ public class JsonConfig {
     // db config
     String dbUser;
     String dbPass;
+    // twitter stuff
+    String key;
+    String secretkey;
+    String access;
+    String secretaccess;
+    // bot config
+    String ownerid;
+    String prefix;
+    String devprefix;
+    // bot admins
+    List<String> admins;
+    // twitter user ids
+    long[] groupa;
+    long[] groupb;
+    // twitter channels
+    String achan;
+    String bchan;
+    String devchan;
+    // twitter toggle
+    boolean twittertog;
+    // slack shit
+    String slackbot;
+    boolean enslack;
 }
