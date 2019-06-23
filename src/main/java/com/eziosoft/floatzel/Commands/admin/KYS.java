3c88972835297ec54ea6ab9ccdd2a70e51bc9ad4
jon <jedijon567@gmail.com>
2019-06-23 17:34:34 -0400

    patchy mc patch face

--- src/main/java/com/eziosoft/floatzel/Commands/admin/KYS.java
+++ src/main/java/com/eziosoft/floatzel/Commands/admin/KYS.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class KYS extends FCommand {
     
     public KYS() {
-        name = "kys";
+        name = "shutdown";
         ownerCommand = true;
         description = "Shuts down the bot";
         category = owner;
@@ -18,11 +18,20 @@ public class KYS extends FCommand {
     
     @Override
     protected void cmdrun(CommandEvent commandEvent) {
-        commandEvent.getChannel().sendMessage("\uD83D\uDD2B").queue();
-        commandEvent.getChannel().sendMessage("\uD83C\uDDE7 \uD83C\uDDE6 \uD83C\uDDF3 \uD83C\uDDEC").queue(m -> {
-            Floatzel.jda.shutdown();
-            Floatzel.twitterManager.shutdown();
-            System.exit(1);
-        });
+        if (argsplit[0].equals("-lolxd")) {
+            commandEvent.getChannel().sendMessage("\uD83D\uDD2B").queue();
+            commandEvent.getChannel().sendMessage("\uD83C\uDDE7 \uD83C\uDDE6 \uD83C\uDDF3 \uD83C\uDDEC").queue(m -> {
+                Floatzel.jda.shutdown();
+                Floatzel.twitterManager.shutdown();
+                System.exit(1);
+            });
+        } else {
+            commandEvent.getChannel().sendMessage("aw man the fucking government took away my gun ;-;").queue();
+            commandEvent.getChannel().sendMessage("Screw this shit, im out, bye").queue(m -> {
+                Floatzel.jda.shutdown();
+                Floatzel.twitterManager.shutdown();
+                System.exit(1);
+            });
+        }
     }
 }
