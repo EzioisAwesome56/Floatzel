5a9c2a78ddac1346cbc041502b09d12b3afbd741
jon <jedijon567@gmail.com>
2019-08-20 19:25:47 -0400

    lol

--- src/main/java/com/eziosoft/floatzel/Commands/Other/Ping.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Ping.java
@@ -16,6 +16,7 @@ public class Ping extends FCommand {
     protected void cmdrun(CommandEvent event) throws Exception {
         // this totally wasnt stole from kekbot nope
         event.getChannel().sendMessage("Pinging... ").queue(m -> m.editMessage("\uD83C\uDFD3 Pong! `" + event.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS)+ "ms`" +
-                "\n\uD83D\uDC93 Heartbeat: `" + event.getJDA().getPing() + "ms`").queue());
+                "\n\uD83D\uDC93 Heartbeat: `" + event.getJDA().getPing() + "ms`\n" +
+                "Command totally not stolen from kekbot nope").queue());
     }
 }
