5aaad681b84dee9f94eca0055b82e9c64a8db18b
jon <jedijon567@gmail.com>
2019-08-20 19:19:50 -0400

    add ping command

---
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Ping.java
@@ -0,0 +1,21 @@
+package com.eziosoft.floatzel.Commands.Other;
+
+import com.eziosoft.floatzel.Commands.FCommand;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import java.time.temporal.ChronoUnit;
+
+public class Ping extends FCommand {
+    public Ping(){
+        name = "ping";
+        category = other;
+        description = "find out how slow the bot is";
+    }
+
+    @Override
+    protected void cmdrun(CommandEvent event) throws Exception {
+        // this totally wasnt stole from kekbot nope
+        event.getChannel().sendMessage("Pinging... ").queue(m -> m.editMessage("\uD83C\uDFD3 Pong! `" + event.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS)+ "ms`" +
+                "\n\uD83D\uDC93 Heartbeat: `" + event.getJDA().getPing() + "ms`").queue());
+    }
+}
