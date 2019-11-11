ff44c5f02bb7615197964ae2e13b1077fee1962e
jon <jedijon567@gmail.com>
2019-11-11 16:46:48 -0500

    i forgot to fully remove slack...ugh

--- src/main/java/com/eziosoft/floatzel/Commands/Spam/Spam.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Spam/Spam.java
@@ -1,8 +1,6 @@
 package com.eziosoft.floatzel.Commands.Spam;
 
 import com.eziosoft.floatzel.Commands.FCommand;
-import com.eziosoft.floatzel.CommdLogic.SpamLogic;
-import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import org.apache.commons.lang3.StringUtils;
 
@@ -18,8 +16,22 @@ public class Spam extends FCommand {
     @Override
     protected void cmdrun(CommandEvent event) {
         String character = event.getArgs();
-        // send back whatever message comes back
-        event.reply(SpamLogic.makeMessage(character));
+        // taken from the old spam.java
+        String[] bad = {"`", "*", "_"};
+        // main code
+        if (character.length() == 0) {
+            event.reply("You didn't fucking give me anything to spam dumbass!");
+            return;
+        } else if (character.length() > 1) {
+            event.reply("That is not one single character dumbass!");
+            return;
+        } else {
+            if (Arrays.stream(bad).anyMatch(s -> s.equals(character))) {
+                event.reply("you can't use that fucking character dumbass");
+                return;
+            }
+        }
+        event.reply(StringUtils.repeat(character, 2000));
     }
 }
 
