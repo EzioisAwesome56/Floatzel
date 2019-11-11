6978e17ce69ccd0d4eb212eab88b1530db7d6173
jon <jedijon567@gmail.com>
2019-11-11 16:48:28 -0500

    fix stats command

--- src/main/java/com/eziosoft/floatzel/Commands/Other/Stats.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Other/Stats.java
@@ -1,7 +1,6 @@
 package com.eziosoft.floatzel.Commands.Other;
 
 import com.eziosoft.floatzel.Commands.FCommand;
-import com.eziosoft.floatzel.CommdLogic.StatsLogic;
 import com.eziosoft.floatzel.Floatzel;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
@@ -14,6 +13,35 @@ public class Stats extends FCommand {
 
     @Override
     protected void cmdrun(CommandEvent event){
-        event.getChannel().sendMessage(StatsLogic.makeMessage(false)).queue();
+        // put this shit back where it belongs!
+        // start forming the stats message
+        String stats = "```";
+        stats = stats + "Floatzel Stats\n";
+        // version
+        stats = stats + "Version: " + Floatzel.version + "\n";
+        // java vendor
+        stats = stats + "Java Vendor: " + System.getProperty("java.vendor") + "\n";
+        // java version
+        stats = stats + "Java Version: " + System.getProperty("java.version") + "\n";
+        // operating system
+        stats = stats + "Host OS: " + System.getProperty("os.name") + "\n";
+        // operating system version
+        stats = stats + "OS Version: " + System.getProperty("os.version") + "\n";
+        // operating system architechure
+        stats = stats + "OS Artchitecture: " + System.getProperty("os.arch") + "\n";
+        // RAMMMMMM
+        stats = stats + "Total RAM: "+ Double.toString(((double)Runtime.getRuntime().totalMemory() / (double)(1024.0 * 1024.0))) + " MB\n";
+        // free mem
+        stats = stats + "Free RAM: "+ Double.toString(((double)Runtime.getRuntime().freeMemory() / (double)(1024.0 * 1024.0))) + " MB\n";
+        // used ram
+        stats = stats + "Used RAM: "+ Double.toString(((double)(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())) / (double)(1024.0 * 1024)) + " MB\n";
+        // max ram
+        stats = stats + "Max RAM: " + Double.toString(((double)Runtime.getRuntime().maxMemory() / (double)(1024.0 * 1024.0))) + " MB\n";
+        // processors
+        stats = stats + "Accessible Processors: "+ Integer.toString(Runtime.getRuntime().availableProcessors()) + "\n";
+        // end it
+        stats = stats + "```";
+        // return the completed message
+        event.getChannel().sendMessage(stats).queue();
     }
 }
