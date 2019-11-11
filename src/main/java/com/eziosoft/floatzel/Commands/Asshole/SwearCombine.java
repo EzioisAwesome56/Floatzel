859070b7847a62ba785f8e07e305fefff0ffca4c
jon <jedijon567@gmail.com>
2019-11-11 16:50:43 -0500

    free gensent from slack

--- src/main/java/com/eziosoft/floatzel/Commands/Asshole/SwearCombine.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Asshole/SwearCombine.java
@@ -1,7 +1,6 @@
 package com.eziosoft.floatzel.Commands.Asshole;
 
 import com.eziosoft.floatzel.Commands.FCommand;
-import com.eziosoft.floatzel.CommdLogic.SwearCombineLogic;
 import com.eziosoft.floatzel.Floatzel;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
@@ -16,6 +15,24 @@ public class SwearCombine extends FCommand {
 
     @Override
     protected void cmdrun(CommandEvent event){
-        event.getChannel().sendMessage(SwearCombineLogic.makeMessage()).queue();
+        // why did i think the slack support was a good idea? it wasnt.
+        Random random = new Random();;
+        String[] words = {"fuck", "ass", "shit", "what", "die", "fucking", "hecking", "you", "nibba", "gay ass pingu", "fucktart", "asswipe", "fuckhead", "asshat", "shitface",
+                "assholefuckface", "dickface", "fucktard", "fuckfart", "nibba"};
+        int maxnumb = words.length;
+        int sentlong = random.nextInt(10) + 1;
+        int count = 0;
+        String msg = "";
+        // form the sentance
+        while (count != sentlong){
+            msg = msg + words[random.nextInt(maxnumb)] + " ";
+            count = count + 1;
+        }
+        // return it
+        if (!Floatzel.joke) {
+            event.getChannel().sendMessage(msg).queue();
+        } else {
+            event.reply("** **");
+        }
     }
 }
