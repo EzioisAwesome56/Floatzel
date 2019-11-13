960de6c3cdb72cf9683f80b7ace17cfec79443b7
jon <jedijon567@gmail.com>
2019-11-13 17:56:45 -0500

    new: brainfuck support

---
+++ src/main/java/com/eziosoft/floatzel/Util/Brainfuck.java
@@ -0,0 +1,9 @@
+package com.eziosoft.floatzel.Util;
+
+public class Brainfuck {
+
+    // Eziosoft Brainfuck interpreter
+    public static String ParseBrainFuck(String fuck){
+        // do shit here
+    }
+}
