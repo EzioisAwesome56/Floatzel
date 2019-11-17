5b003878471478baa1883c0e73f05d423fa7780b
jon <jedijon567@gmail.com>
2019-11-16 21:15:11 -0500

    get rid of random and use random provided in fcommand

--- src/main/java/com/eziosoft/floatzel/Commands/Secret/Gay.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Secret/Gay.java
@@ -7,7 +7,6 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 import java.util.Random;
 
 public class Gay extends FCommand {
-    private final Random random = new Random();
     // secret command so we dont need description stuff
     public Gay(){
         name = "gay";
@@ -17,7 +16,6 @@ public class Gay extends FCommand {
 
     @Override
     protected void cmdrun(CommandEvent event){
-        Random random = new Random();
         String[] gay = {"The person above is very fucking gay", "Above is the gayiest thing ever", "the person above will eat his own virginia/dick"};
         //  call person above gay
         event.getMessage().delete().queue(m -> event.getChannel().sendMessage(gay[random.nextInt(gay.length)]).queue());
