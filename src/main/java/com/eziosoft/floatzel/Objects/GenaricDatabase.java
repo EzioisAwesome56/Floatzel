5ab3b4aa19110498d63d0fdb549e44e3ad60aa1f
Jon <jedijon567@gmail.com>
2020-12-22 20:48:05 -0500

    modloader code

---
+++ src/main/java/com/eziosoft/floatzel/Objects/GenaricDatabase.java
@@ -0,0 +1,14 @@
+package com.eziosoft.floatzel.Objects;
+
+public interface GenaricDatabase {
+
+    void Conninfo(String info);
+
+    String getProfile(String id);
+
+    void saveProfile(String json);
+
+    void initDatabase();
+
+
+}
--- src/main/java/com/eziosoft/floatzel/Objects/ModLoader.java
+++ src/main/java/com/eziosoft/floatzel/Objects/ModLoader.java
@@ -1,10 +1,9 @@
 package com.eziosoft.floatzel.Objects;
 
+import com.google.gson.Gson;
 import org.apache.commons.io.FilenameUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -16,9 +15,11 @@ public class ModLoader {
 
     private File[] mfolder;
 
-    private String[] mainclasses;
+    private Mod[] mods;
 
     private URLClassLoader child;
+
+    private static Gson gson = new Gson();
 
     public int getTotalMods(){
         return mfolder.length;
@@ -35,6 +36,7 @@ public class ModLoader {
         this.mfolder = temp.listFiles();
         // form into list
         ArrayList<URL> ulist = new ArrayList<URL>();
+        ArrayList<Mod> modlist = new ArrayList<Mod>();
         for (int h = 0; h < getTotalMods(); h++){
             if (mfolder[h].isDirectory()){
                 System.out.println("Mod \"" + mfolder[h].toString() + "\" is a Directory! Skipping...");
@@ -44,18 +46,24 @@ public class ModLoader {
                 System.out.println("Mod \"" + mfolder[h].toString() + "\" is not a JAR file! Skipping...");
                 continue;
             }
-            // past the error traps? get main class from jar
+            // past the error traps? get "mod" object from jar's json
             JarFile jar = new JarFile(mfolder[h]);
+            Reader read = new InputStreamReader(jar.getInputStream(jar.getJarEntry("modinfo.json")));
+            Mod tempmod = gson.fromJson(read, Mod.class);
+            // store it away
+            modlist.add(tempmod);
+            // also add mod url to the url list
             ulist.add(mfolder[h].toURI().toURL());
+            // broadcast that mods have been found
+            System.out.println("Mod \"" + mfolder[h].toString() + "\" has been queued for load");
         }
 
         this.child = new URLClassLoader(
                 ulist.toArray(new URL[]{}),
                 ModLoader.class.getClassLoader());
-    }
 
-    public Object loadDatabase(){
-        // do cool things here
+        this.mods = modlist.toArray(new Mod[]{});
+        System.out.println("Eziosoft Modloader Initialization complete!");
     }
 
 
