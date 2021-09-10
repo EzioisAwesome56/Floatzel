f1b85e2690930c4aae5680b56aa7ba68a17a2728
Jon <jedijon567@gmail.com>
2021-09-09 23:31:30 -0400

    version bump

--- src/main/java/com/eziosoft/floatzel/Floatzel.java
+++ src/main/java/com/eziosoft/floatzel/Floatzel.java
@@ -117,7 +117,7 @@ public class Floatzel {
         twitterManager = new TwitterManager();
 
         // resume everything else
-        version = !isdev ? "2.6.1" : "2.x Developement";
+        version = !isdev ? "2.7" : "2.x Developement";
          commandClient = new CommandClientBuilder().setOwnerId(conf.getOwnerid()).useHelpBuilder(false).setPrefix(!isdev ? conf.getPrefix() : conf.getDevprefix()).build();
 
 
