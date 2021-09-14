3ea6f7e6127c343ca625b37077ac940783d3a805
Jon <jedijon567@gmail.com>
2021-09-14 16:21:44 -0400

    required for mod compatibility

--- src/main/java/com/eziosoft/floatzel/Commands/FCommand.java
+++ src/main/java/com/eziosoft/floatzel/Commands/FCommand.java
@@ -25,6 +25,7 @@ public abstract class FCommand extends Command {
     public static Category owner = new Category("Owner Commands");
     public static Category sound = new Category("Audio");
     public static Category other = new Category("Other");
+    public static Category asshole = other;
     public static Category waifu = new Category("Waifu");
     public static Category buyshit = new Category("Purchasable Commands");
     public static Category stocks =  new Category("Stock Market");
