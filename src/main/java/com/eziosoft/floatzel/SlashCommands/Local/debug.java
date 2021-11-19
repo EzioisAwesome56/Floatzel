c0687b835811faba69334a84dcee5367ec031a76
Ezio <jedijon567@gmail.com>
2021-11-19 14:15:35 -0500

    put some test code in debug so i can look at it later

--- src/main/java/com/eziosoft/floatzel/SlashCommands/Local/debug.java
+++ src/main/java/com/eziosoft/floatzel/SlashCommands/Local/debug.java
@@ -4,8 +4,16 @@ import com.eziosoft.floatzel.Floatzel;
 import com.eziosoft.floatzel.SlashCommands.FSlashCommand;
 import com.eziosoft.floatzel.SlashCommands.Objects.SlashableCommandEntry;
 import com.eziosoft.floatzel.SlashCommands.SlashActionGroup;
+import net.dv8tion.jda.api.events.Event;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.components.Button;
+import net.dv8tion.jda.api.interactions.components.Component;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 public class debug extends FSlashCommand {
 
@@ -18,8 +26,24 @@ public class debug extends FSlashCommand {
 
     @Override
     public void execute(SlashCommandEvent e) {
-        if (Floatzel.scm.hasSlashAction(new SlashableCommandEntry(SlashActionGroup.OTHER, "pi"))){
-            Floatzel.scm.getSlashAction(new SlashableCommandEntry(SlashActionGroup.OTHER, "pi")).SlashCmdRun(e);
-        }
+        e.getHook().sendMessage("test button").addActionRow(
+                Button.primary("gamer", "test button")
+        ).queue();
+        Floatzel.waiter.waitForEvent(Event.class, p -> {
+            if (p instanceof ButtonClickEvent){
+                boolean a = ((ButtonClickEvent) p).getInteraction().getMember().getUser().getId().equals(e.getMember().getUser().getId());
+                System.out.println(a);
+                return a;
+            } else {
+                return false;
+            }
+        }, a -> {
+            ButtonClickEvent bce = (ButtonClickEvent) a;
+            bce.editButton(null).queue();
+            bce.getHook().editOriginal("You are a pirate!").queue();
+        }, 1, TimeUnit.MINUTES, () -> {
+            e.getHook().deleteOriginal().queue();
+            e.getHook().setEphemeral(true).sendMessage("You took too long!").queue();
+        });
     }
 }
