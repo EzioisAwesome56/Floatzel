d82e996ff3770d2f44bd53a15c96077fd70001f6
Ezio <jedijon567@gmail.com>
2021-12-03 14:27:25 -0500

    add some things to the settings manager

--- src/main/java/com/eziosoft/floatzel/Objects/GuildSettingsManager.java
+++ src/main/java/com/eziosoft/floatzel/Objects/GuildSettingsManager.java
@@ -1,6 +1,7 @@
 package com.eziosoft.floatzel.Objects;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GuildSettingsManager {
@@ -8,5 +9,17 @@ public class GuildSettingsManager {
 
     public GuildSettingsManager(){
         System.out.println("Floatzel GuildSettings Manager now starting up...");
+        GuildSettings f = new GuildSettings();
+    }
+
+    private class GuildSettings {
+        @PrimaryKey
+        private String guildId;
+
+        private boolean commandsEnabled;
+        private String prefix;
+        private List<String> notAllowedChans;
+
+        public GuildSettings(){};
     }
 }
