e0e89d345d16d58e841dceb0ae337fdf9260e8b2
EzioisAwesome56 <jedijon567@gmail.com>
2022-01-05 19:51:32 -0500

    hotfix 2: redo tweetutil, update format xm

--- src/main/java/com/eziosoft/floatzel/Commands/Owner/TweetUtils.java
+++ src/main/java/com/eziosoft/floatzel/Commands/Owner/TweetUtils.java
@@ -6,9 +6,12 @@ import com.eziosoft.floatzel.Util.Database;
 import com.eziosoft.floatzel.Util.Utils;
 import com.google.gson.Gson;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import org.apache.commons.io.IOUtils;
 
-import javax.xml.crypto.Data;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TweetUtils extends FCommand {
 
@@ -25,17 +28,52 @@ public class TweetUtils extends FCommand {
     @Override
     protected void cmdrun(CommandEvent event) throws Exception {
         if (argsplit[0].equals("dump")){
-            ArrayList<Tweet> list = new ArrayList<>();
+            wrapper wrap = new wrapper();
             int total = Database.dbdriver.totalTweets();
-            for (int x = 1; x <= total; x++){
+            for (int x = 1; x < total; x++){
                 // put every tweet into an array
-                list.add(new Tweet(Database.dbdriver.loadTweet(x).getText(), x));
+                wrap.getContent().add(Database.dbdriver.loadTweet(x).getText());
             }
-            event.getChannel().sendMessage("tweets dumped!").addFile(gson.toJson(list).getBytes("UTF-8"), "tweets.json").queue();
+            event.getChannel().sendMessage("tweets dumped!").addFile(gson.toJson(wrap).getBytes("UTF-8"), "tweets.json").queue();
         } else if (argsplit[0].equals("import")){
-            event.reply("do things here");
+            if (event.getMessage().getAttachments().size() < 1){
+                event.reply("Error: please provide json to import!");
+                return;
+            }
+            // get stream
+            InputStream stream = event.getMessage().getAttachments().get(0).retrieveInputStream().join();
+            String json = IOUtils.toString(stream, StandardCharsets.UTF_8);
+            stream.close();
+            event.reply("Now importing data...");
+            wrapper wrap = gson.fromJson(json, wrapper.class);
+            System.out.println("Importing tweets...");
+            int current = Database.dbdriver.totalTweets();
+            int total = 0;
+            for (String s : wrap.getContent()){
+                Database.dbdriver.saveTweet(new Tweet(s, current));
+                current++;
+                total++;
+            }
+            event.reply("Imported "+ total + " tweets");
         } else {
             event.reply("This command requires either `dump` or `import` to be supplied with the command\nimport requires a json file with previously dumped tweets to be attached");
+        }
+    }
+
+
+    static class wrapper{
+        private List<String> content;
+
+        public List<String> getContent() {
+            return content;
+        }
+
+        public wrapper(){
+            this.content = new ArrayList<>();
+        }
+
+        public void AddTweet(String s){
+            this.content.add(s);
         }
     }
 }
